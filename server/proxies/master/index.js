31900d715625a89cfdcbf1e71655158fcbd03fa1
kiwialec <alec@allotmentdigital.com>
2020-08-29 12:40:53 +0100

    clean up some tech debt

--- server/proxies/master/index.js
+++ server/proxies/master/index.js
@@ -62,27 +62,13 @@ module.exports = class Master {
             self._mitm = new Mitm({cert, key});
         }
 
-        // Config server
+      
         self._server = http.createServer();
-
-        // self._server.on('request', request);
-        // self._server.on('connect', connect);
-
-        self._hackServer = http.createServer();
-        self._hackServer.on('request', hackedRequest);
+        self._server.on('request', request);
 
         ////////////
-        function hackedRequest(req, res){
+        function request(req, res){
     
-            var trueUrl = req.url.substring(1)
-            
-            var parse = new URL(trueUrl)
-            req.headers.host = parse.host
-            req.url = trueUrl
-            return requestImpl(req, res);
-        }
-        function request(req, res) {
-            // Check auth
             if (self._token) {
                 if (!req.headers['proxy-authorization'] || req.headers['proxy-authorization'] !== self._token) {
                     return writeEndRequest(res, 407, '[Master] Error: Wrong proxy credentials', {
@@ -92,6 +78,11 @@ module.exports = class Master {
                 }
             }
 
+            var trueUrl = req.url.substring(1)
+            var parse = new URL(trueUrl)
+            req.headers.host = parse.host
+            req.url = trueUrl
+            
             return requestImpl(req, res);
         }
 
@@ -131,7 +122,7 @@ module.exports = class Master {
 
             // Update headers
             //instance.updateRequestHeaders(req.headers);
-
+            delete req.headers['proxy-authorization'];
             // Make request
             const proxyOpts = _.merge(createProxyOpts(req.url), {
                 method: req.method,
@@ -211,141 +202,6 @@ module.exports = class Master {
             }
         }
 
-        function connect(req, socket) {
-            // Check auth
-            if (self._token) {
-                if (!req.headers['proxy-authorization'] || req.headers['proxy-authorization'] !== self._token) {
-                    return writeEndSocket(socket, 407, '[Master] Error: Wrong proxy credentials', {
-                        'Proxy-Authenticate': 'Basic realm="Scrapoxy"',
-                        'Connection': 'close',
-                    });
-                }
-            }
-
-            // Log errors
-            req.on('error',
-                (err) => {
-                    winston.error('[Master] Error: request error from client (%s %s on instance %s):', req.method, req.url, instance.toString(), err);
-                }
-            );
-
-            socket.on('error',
-                (err) => {
-                    winston.error('[Master] Error: socket error from client (%s %s on instance %s):', req.method, req.url, instance.toString(), err);
-                }
-            );
-
-            if (self._mitm) {
-                return self._mitm.connect(socket, (rq, rs) => requestImpl(rq, rs));
-            }
-
-            // Check requested url against rules
-            if (isUrlForbidden(req.url)) {
-                return writeEndSocket(socket, 407, '[Master] Error: request is forbidden by url rule');
-            }
-
-            // Trigger scaling if necessary
-            self._manager.requestReceived();
-
-            // Get domain
-            const target = parseTarget(req.url),
-                uri = domain.convertHostnamePathToUri(target.hostname, target.url),
-                basedomain = domain.getBaseDomainForUri(uri);
-
-            // Find instance
-            const forceName = req.headers['x-cache-proxyname'],
-                instance = self._manager.getNextRunningInstanceForDomain(basedomain, forceName);
-
-            if (!instance) {
-                return writeEndSocket(socket, 407, '[Master] Error: No running instance found');
-            }
-
-            // Cannot update headers because SSL is encrypted
-
-            // Make request
-            const proxyParameters = instance.proxyParameters;
-            const proxyOpts = {
-                host: proxyParameters.hostname,
-                port: proxyParameters.port,
-                method: 'CONNECT',
-                path: req.url,
-                agent: self._agent, // Don't use the proxy agent
-                headers: {},
-            };
-
-            const proxy_req = http.request(proxyOpts);
-
-            proxy_req.on('error', (err) => {
-                winston.error('[Master] Error: request error from client (%s %s on instance %s):', req.method, req.url, instance.toString(), err);
-
-                return writeEndSocket(socket, 500, `[Master] Error: request error from target (${req.method} ${req.url} on instance ${instance.toString()}): ${err.toString()}`);
-            });
-
-            // Start timer
-            const start = process.hrtime();
-
-            proxy_req.on('connect', (instance_req, proxy_socket) => {
-                proxy_socket.on('error', (err) => {
-                    winston.error('[Master] Error: response error from target (%s %s on instance %s):', req.method, req.url, instance.toString(), err);
-
-                    return writeEndSocket(socket, 500, `[Master] Error: response error from target (${req.method} ${req.url} on instance ${instance.toString()}): ${err.toString()}`);
-                });
-
-                proxy_socket.on('end', () => {
-                    // Stop timer and record duration
-                    const duration = process.hrtime(start);
-
-                    self._stats.requestEnd(
-                        duration,
-                        proxy_socket._bytesDispatched,
-                        proxy_socket.bytesRead
-                    );
-
-                    instance.incrRequest();
-                });
-
-                socket.write('HTTP/1.1 200 Connection established\r\n\r\n');
-
-                proxy_socket.pipe(socket).pipe(proxy_socket);
-            });
-
-            proxy_req.end();
-
-
-            ////////////
-
-            function parseTarget(reqUrl) {
-                if (!reqUrl) {
-                    return {
-                        hostname: '',
-                        port: '',
-                    };
-                }
-
-                const part = reqUrl.split(':');
-                if (part.length !== 2) {
-                    return {
-                        hostname: '',
-                        port: '',
-                    };
-                }
-
-                const hostname = part[0],
-                    port = parseInt(part[1]);
-
-                if (!hostname ||
-                    hostname.length <= 0 ||
-                    !port) {
-                    return {
-                        hostname: '',
-                        port: '',
-                    };
-                }
-
-                return {hostname, port};
-            }
-        }
-
         function isUrlForbidden(url) {
             let rUrl;
             if (!url) {
@@ -410,9 +266,7 @@ module.exports = class Master {
 
 
     listen() {
-        return Promise.all([
-            new Promise((resolve, reject) => {
-
+        return new Promise((resolve, reject) => {
                 this._server.listen(this._config.port, (err) => {
                     if (err) {
                         return reject(new Error(`[Master] Cannot listen at port ${this._config.port} : ${err.toString()}`));
@@ -422,25 +276,13 @@ module.exports = class Master {
 
                     return resolve();
                 });
-            }),
-            new Promise((resolve, reject) => {
-                this._hackServer.listen(8887, (err) => {
-                    if (err) {
-                        return reject(new Error(`[Master] Cannot listen at port 8887 : ${err.toString()}`));
-                    }
-
-                    winston.info('Proxy is listening at http://localhost:%d', 8887);
-
-                    return resolve();
-                });
             })
-        ])
+        
     }
 
 
     shutdown() {
         winston.debug('[Master] shutdown');
-        this._hackServer.close();
         this._server.close();
     }
 };
