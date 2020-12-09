534c2776a689b369d5b59778417cc7ee14f41d49
kiwialec <alec@allotmentdigital.com>
2020-12-09 09:28:50 +0000

    change

--- tools/install/proxy_server.js
+++ tools/install/proxy_server.js
@@ -28,28 +28,16 @@ socket.on("connect", () => {
   });
 
 socket.on('req', (obj, fn) => {
-        bcrypt.compare(`${JSON.stringify(obj.req)}-REQUESTSECRET-${obj.expiry}`, obj.hash).then(async result => {
-            if(result === true && new Date().getTime() < obj.expiry && obj.expiry < (new Date().getTime()+(60*60*1000))){
-                console.log("CLIENT REQ WILL SUCCEED",obj.req.action)
-                switch(obj.req.action){
-                    case "ping":
-                        fn({status: true, message: "pong"});
-                    break;
-                    case "scrape":
-                        var response = await request(obj.req)
-                        fn(response)
-                    break;
-                }
-            }else{
-                console.log(
-                    "CLIENT REQ WILL FAIL", 
-                    result, 
-                    (new Date().getTime() < obj.expiry), 
-                    (obj.expiry < (new Date().getTime()+(60*60*1000)))
-                )
-            }
-        
-        });
+    switch(obj.req.action){
+        case "ping":
+            fn({status: true, message: "pong"});
+        break;
+        case "scrape":
+            var response = await request(obj.req)
+            fn(response)
+        break;
+    }
+         
 })
 
 
